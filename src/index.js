c2657ca603d6bdc056df155967eec6c0f1c206d8
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-02-03 12:14:25 +0200

    Add index.js to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+import getData from './modules/ApiData.js';
+import createCardItem from './modules/CardItem.js';
+
+// const movieIdd = 139;
+
+const renderItems = async () => {
+  const itemsData = await getData();
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < itemsData.length; i++) {
+    createCardItem(itemsData[i]);
+  }
+};
+
+renderItems();
